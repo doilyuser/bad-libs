513ebd2eefa9ff9cd34d81a7c66b5d7e1e1928e3
Louis Dyer <111328948+doilyuser@users.noreply.github.com>
2023-03-03 12:13:23 +1300

    router get no longer goes to main.css

--- server/server.js
+++ server/server.js
@@ -2,9 +2,8 @@ const express = require('express')
 const hbs = require('express-handlebars')
 const fs = require('node:fs/promises')
 
+const routes = require('./routes')
 const server = express()
-const router = require('./routes')
-
 // Server configuration
 const publicFolder = __dirname + '/public'
 server.use(express.static(publicFolder))
@@ -16,8 +15,8 @@ server.set('view engine', 'hbs')
 server.set('views', __dirname + '/views')
 
 // Your routes/router(s) should go here
-server.use('/create', router)
-server.use('/story', router)
+server.use('/create', routes)
+server.use('/story', routes)
 
 // server.use('/theme', router)
 
